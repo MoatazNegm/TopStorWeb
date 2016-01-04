cd247009aff18e5a847ec2cf17f11c06454aac87
Mezo Negm <mezo@quickstor.com>
2016-01-04 22:21:40 +0200

    adding language selector 2

--- Login.php
+++ Login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php $men=2; include "header2.html";  session_start();  session_commit(); ?>
 	<div class="row">
-		<div class="col-sm-offset-5 col-sm-3">
+		<div class="col-sm-offset-4 col-sm-4">
 			<h4><strong></strong></h4>
 				<form action="Login2.php" method="post">
 				<input type="text" name="userName" class="form-control chat-input centry " placeholder="username">
@@ -10,7 +10,7 @@
 				<input type="hidden" name="idd" value="<?php print session_id();?>" >
 				</br>
 				<div class="wrapper">
-					<button type="submit" id="logpls" class="col-sm-offset-4 btn btn-primary btn-md" >login
+					<button type="submit" id="logpls" class="col-sm-offset-5 btn btn-primary btn-md" >login
 						<i class="fa fa-sign-in"></i> 
 					</button>
 				</div>
