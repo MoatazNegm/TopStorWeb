ce7fca3da685aa846056b62c7a00248b008044de
Mezo <sales@quickstor.net>
2016-07-23 19:26:06 +0200

    fixing

--- status.php
+++ status.php
@@ -282,6 +282,7 @@
 		if( traffictime == trafficnewtime && requeststats==0) { //console.log("traffic not changed");
 			$.get("requeststats.php", { date: datern, time: 0 });
 			requeststats=1;
+			console.log("requesint");
 		} 
 		else {
 			traffictime = trafficnewtime 			
