9402bf343050b9a5e1ac8feb7c328caff8adf39a
Mezo <sales@quickstor.net>
2016-07-23 10:10:48 +0200

    fixing

--- Getstats.sh
+++ Getstats.sh
@@ -1,10 +1,9 @@
-#! /bin/sh
+#! /bin/bash
 date=`echo $@ | awk '{print $1}'`
-time=`echo $@ | awk '{print $2}'`
+time=`echo $@ | awk '{print $2}'`;
 found=1
 seconds=`echo $time | awk -F':' '{print $3}'`;
 seconds=$((59-seconds));
-echo $seconds
 firsttime=`cat Data/*$date*.tab | grep -v \# | head -n 1 | awk '{print $2}'`
 printf "$time\n$firsttime\n" | sort -u | tail -n 1 | grep "$firsttime"
 if [ $? -eq  0 ]; 
@@ -18,7 +17,7 @@ while [ $found -ge 1 ]; do
  cat Data/*$date*.tab | grep "$time" > /dev/null
  if [ $? -eq 0 ] 
  then
-  cat  Data/*$date*.tab | grep -v \# | sort -u  | awk  "BEGIN{flag=0;count=0} /$time/{flag=1}{if (flag > 0 ) { print; count+=1; } } ";
+  stats=`cat  Data/*$date*.tab | grep -v \# | sort -u  | awk  "BEGIN{flag=0;count=0} /$time/{flag=1}{if (flag > 0 ) { print; count+=1; } } "`
   found=0;
  else
   time=`date --date=${time}' seconds' +%T`
