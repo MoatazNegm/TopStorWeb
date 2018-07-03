2ae0461d5585ddedf0bd37f1c44d13c0ebe2c37c
dkljf <dlksjf@dlafjk>
2018-07-03 18:16:30 +0200

    dalfjd

---
+++ etcdget2.py
@@ -0,0 +1,33 @@
+#!/bin/python3.6
+import subprocess,sys
+import json
+
+key=sys.argv[1]
+try:
+ prefix=sys.argv[2]
+except:
+ prefix=''
+endpoints=''
+data=json.load(open('Data/runningetcdnodes.txt'));
+for x in data['members']:
+ endpoints=endpoints+str(x['clientURLs'])[2:][:-2]+','
+cmdline=['etcdctl','--endpoints='+endpoints,'get',key,prefix]
+result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+ilist=[]
+try:
+ if(prefix !=''):
+  mylist=str(result.stdout)[2:][:-3].split('\\n')
+  mylist=zip(mylist[0::2],mylist[1::2])
+  hostid=0
+  hosts=[]
+  for x in mylist:
+   ll=[]
+   ll.append(x[1])
+   hostsdic={'id':str(hostid),'name':x[0],'prop':x[1]}
+   hosts.append(hostsdic)
+  print(str(hosts).replace('"','').replace("'",'"'))
+ else:
+  print(str(result.stdout).split(key)[1][2:][:-3].replace("'",'"'))
+ 
+except:
+ print('-1')
