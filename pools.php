184bb72f4b587a0bb31f8b2dcb4074687c6518a8
m22 <m22@dlafjkd.comd>
2018-09-30 05:21:30 +0200

    dlafkj

--- pools.php
+++ pools.php
@@ -1100,7 +1100,6 @@ function refreshall() { //check pool status
 						}
 						else { clickdisk="javascript:diskclick('"+kk+"')"; clickdisk="href="+clickdisk; imgf="disk-image.png" 
 						}	
-				$("#"+diskdiv).append('<div class="disks '+disks[kk]['host']+' '+disks[kk]['pool']+' '+disks[kk]["status"]+' '+disks[kk]["changeop"]+'" ><a id="i'+kk+'"'+clickdisk+' > <img class="img-fluid '+ diskimg+' disk'+kk+'" src="assets/images/'+imgf+'" alt="can\'t upload disk images"></a><a '+clickdisk+'><p class="psize">'+disks[kk]["size"]+'</p></a><p class="pimage">disk'+kk+'</p><p class="pimage p'+disks[kk]["status"]+'">'+disks[kk]["status"]+'</p><p class="pimage">'+disks[kk]["grouptype"]+'</p><p class="pimage">'+disks[kk]["fromhost"]+'</p>')
 				  $("#"+diskdiv2).append(
       '<div id="diskrow'+disks[kk].pool+'" class="col-1  disks '+disks[kk]['host']+' '+disks[kk]['pool']+' '+disks[kk]['status']+' '+disks[kk]['changeop']+'">'
       +'   <div class="a413">'
