cd9e69e0d0ac1f408360fa65e854f948f638b5b5
dlsfajs <dljfdfa>
2021-04-11 21:58:17 +0300

    djaf;

--- logqueue.py
+++ logqueue.py
@@ -7,7 +7,6 @@ from socket import gethostname as hostname
 from sendhost import sendhost
 def queuethis(*args):
  z=[]
- knowns=[]
  myhost=hostname()
  dt=datetime.datetime.now().strftime("%m/%d/%Y")
  tm=datetime.datetime.now().strftime("%H:%M:%S")
@@ -18,19 +17,12 @@ def queuethis(*args):
  with open('/root/logqueuetmp','w') as f:
   f.write(str(z))
  leaderinfo=get('leader','--prefix')
- knowninfo=get('known','--prefix')
- leaderip=leaderinfo[0][1]
- for k in knowninfo:
-  knowns.append(k[1])
+ leaderip = leaderinfo[0][1]
  print('leader',leaderip) 
- print('knowns',knowns) 
  msg={'req': 'queue', 'reply':z}
  print('sending', leaderip, str(msg),'recevreply',myhost)
  sendhost(leaderip, str(msg),'recvreply',myhost)
- #for k in knowninfo:
- # sendhost(k[1], str(msg),'recvreply',myhost)
- # knowns.append(k[1])
- #print('logqueue z=',z)
-
+ 
 if __name__=='__main__':
+ #queuethis('ddlrt.py','start','system')
  queuethis(*sys.argv[1:])
