8d4bee559d8131a013e929af4007f20e399a8bb4
dlsfajs <dljfdfa>
2021-04-23 23:36:01 +0300

    fixing

--- dashboarddev2/dist/js/QNodes.js
+++ dashboarddev2/dist/js/QNodes.js
@@ -1,6 +1,24 @@
 //input mask bundle ip address
+function memberclick(){
+    name=$(this).attr('data-htname');
+    console.log('hihihi',name)
 
+    if($(this).children('img').hasClass("SelectedFreered") > 0 ) {
+        $(this).children('img').removeClass("SelectedFreered")
+        $(this).children('img').addClass("SelectedFreewhite");
+        selhosts="";
+        $("#RhostForget").attr('disabled',true);
+    } else {
+      $('img.server').removeClass("SelectedFreered")
+      $('img.server').addClass("SelectedFreewhite");
+      $(this).children('img').removeClass("SelectedFreewhite")
+      $(this).children('img').addClass("SelectedFreered");
+        selhosts=name;
+        $("#RhostForget").attr('disabled',false);
+    }
+}
 
+$(".img-clck").click(memberclick);
 var example1_filter = $("#example1_filter");
 $("#example1").DataTable({
     "responsive": true, "lengthChange": true, "autoWidth": true, "info":true,
--- dashboarddev2/dist/js/Qmain.js
+++ dashboarddev2/dist/js/Qmain.js
@@ -2,4 +2,3 @@ $(".main-sidebar").css("background","#131010")
 var ipv4_address = $(".ipaddress");
 ipv4_address.inputmask();
 //Initialize Select2 Elements
-  
