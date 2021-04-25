629b309f6ac1ac5953921f2e91c231476ddbbe17
dlsfajs <dljfdfa>
2021-04-26 00:58:41 +0300

    fixing

--- dashboarddev2/dist/js/Qsnapshots.js
+++ dashboarddev2/dist/js/Qsnapshots.js
@@ -1,42 +1,11 @@
 $(function () {
+  $('#timepicker').datetimepicker({
+    format: 'LT'
+  })
   /* ChartJS
    * -------
    * Here we will create a few charts using ChartJS
    */
-  //-------------
-  //- PIE CHART -
-  //-------------
-  // Get context with jQuery - using jQuery's .get() method.
-  var donutData        = {
-    labels: [
-        'Chrome',
-        'IE',
-        'FireFox',
-        'Safari',
-        'Opera',
-        'Navigator',
-    ],
-    datasets: [
-      {
-        data: [700,500,400,600,300,100],
-        backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
-      }
-    ]
-  }
-  var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
-  var pieData        = donutData;
-  var pieOptions     = {
-    maintainAspectRatio : false,
-    responsive : true,
-  }
-  //Create pie or douhnut chart
-  // You can switch between pie and douhnut using the method below.
-  new Chart(pieChartCanvas, {
-    type: 'pie',
-    data: pieData,
-    options: pieOptions
-  })
-
 
       //---------------------
       //- STACKED BAR CHART -
