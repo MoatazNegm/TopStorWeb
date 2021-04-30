d1db77c072c3edf3c731ed6e6608dd1b1de5759f
dlsfajs <dljfdfa>
2021-05-01 02:38:10 +0300

    fixing

--- dashboarddev2/dist/js/Qmain.js
+++ dashboarddev2/dist/js/Qmain.js
@@ -7,3 +7,22 @@ $(".main-sidebar").css("background","#131010")
 var ipv4_address = $(".ipaddress");
 ipv4_address.inputmask();
 //Initialize Select2 Elements
+jQuery(function($){
+    $('.card-header').each(function(){
+      let $card_header = $(this);
+      let $collapse_element = $card_header.next();
+      $collapse_element.on('show.bs.collapse', function () {
+        let $card_header_top = $card_header.offset().top;
+        let $visible_collapse = $('.collapse.show');
+        if($visible_collapse.length){
+          let $visible_collapse_top = $visible_collapse.offset().top;
+          if($visible_collapse_top < $card_header_top){
+            $card_header_top -= $visible_collapse.height();
+          }
+        }
+        $([document.documentElement, document.body]).animate({
+          scrollTop: $card_header_top
+        });
+      });
+    });
+  });
