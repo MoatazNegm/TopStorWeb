3fc2da8463ce76f6fdbdec894fe96b8251508427
dlsfajs <dljfdfa>
2021-05-17 06:36:13 +0300

    fixing

--- dashboarddev2/dist/js/Qusers.js
+++ dashboarddev2/dist/js/Qusers.js
@@ -17,7 +17,7 @@ var oldcurrentinfo='dlkfajsdl;';
  var mydate;
  var tempvar;
  var allusers="init";
- var allgroups="init";
+ var allgroups={"results": [{"id" : '0', 'text':'NoGroup'}]}
  var allpools="init";
  var selvalues={};
  var grpolddata;
@@ -40,44 +40,86 @@ var oldcurrentinfo='dlkfajsdl;';
  var selhosts="";
  var seldhosts="";
  var modaltill=idletill-120000
+ var userlisttable;
  var myid="<?php echo $_REQUEST['myid'] ?>";
  myidhash=myid;
  var myname="<?php echo $_REQUEST['name'] ?>";
 
 var example1_filter = $("#UserList_filter");
 
-function getgrplst() {
-
-$.ajax({
- url: "api/v1/users/grouplist", 
- type: "GET",
- async: false,
- //beforeSend: function(xhr){xhr.setRequestHeader('Access-Control-Allow-Origin', 'http://10.11.11.241:8080');},
- 
- success: function(data) {  allgroups=data;  console.log('Success!' + allgroups); }
-});
- 
+function groupsrefresh(){
   
+  $('.select2.multiple').select2({
+    ajax: {
+     url: 'api/v1/users/grouplist',
+     dataType: 'json',
+     // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+     type: 'GET',
+     async: false,
+   }
+ });
 }
-function getpoollst() {
-
-  $.ajax({
-   url: "api/v1/pools/poolsinfo", 
-   type: "GET",
-   async: false,
-   //beforeSend: function(xhr){xhr.setRequestHeader('Access-Control-Allow-Origin', 'http://10.11.11.241:8080');},
-   
-   success: function(data) {  allpools=data;  console.log('Success!' + allpools); }
-  });
-   
-    
+function poolsrefresh(){
+ 
+ $('.select2.pool').select2({
+   ajax: {
+    url: 'api/v1/pools/poolsinfo',
+    dataType: 'json',
+    // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+    type: 'GET',
+    async: false,
   }
+ });
+ 
+}
+function userlistrefresh(){
+  userlisttable.ajax.reload(function(){
+    var option;
+    $(".usergroups").each(function(){
+      thisuser=$(this)
+      var grps;
+      assignedgrps = thisuser.data("grps")
+      if(typeof(assignedgrps) == 'number') {
+        grps = [assignedgrps];
+      } else {
+        grps = assignedgrps.split(',');
+      }
+      $.each(grps, function(e,t){
+        if(t !="NoGroup") {
+          var grp = allgroups["results"][t];
+          option = new Option(grp.text, grp.id, true, true)
+          thisuser.append(option).trigger('change');
+        }
+      });
+      // manually trigger the `select2:select` event
+      thisuser.trigger({
+          type: 'select2:select',
+          params: {
+              allgroups: allgroups
+          }
 
+      });
+    });
+    groupsrefresh();
+    $(".select2.usergroups").on('change',function(e){
+      if( $(this).data('grps') != $(this).val().toString()){
+        
+        $("#btn"+$(this).attr('id')).show();
+      }
+      else {
+        $("#btn"+$(this).attr('id')).hide();
+      }
+      
+    });
+    $(".select2.usergroups").trigger('change');
+
+    
+    
+  });
+}
 
 function initUserlist(){
-  getgrplst();
-  getpoollst();
-  $("#UserList").DataTable({
+  userlisttable=$("#UserList").DataTable({
       //"responsive": true, "lengthChange": true, "autoWidth": true, "info":true,
       "order": [[ 1, "desc" ]],
       //"buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
@@ -94,7 +136,8 @@ function initUserlist(){
         {
           data:"groups",
           render: function(data, type, row){
-            return '<select class="select2 multiple usergroups '+row.name+' form-control"  multiple="multiple" onclick="tdisclicked(this)"'
+            return '<select class="select2 multiple usergroups '+row.name+' form-control"' 
+            + ' multiple="multiple" onclick="tdisclicked(this)"' 
             + 'data-grps="'+row.groups+'" value=[0] id="sel'+row.name+'"></select>';
           }
         },
@@ -102,7 +145,7 @@ function initUserlist(){
           data: null,
           render: function(data, type, row){
             return '<button onclick="selbtnclickeduser(this)" id="btnsel'+row.name+'" '
-            + 'type="button" class="btn btn-primary" hidden="True"> update</button>';
+            + 'type="button" class="btn btn-primary" > update</button>';
           }
         },
         {
@@ -130,112 +173,91 @@ function initUserlist(){
         }
       ]
       
-    }).buttons().container().appendTo('#UserList_wrapper .col-6:eq(0)');
-  var option;
-  $(".usergroups").each(function(){
-    thisuser=$(this)
-    assignedgrps = thisuser.data("grps")
-    if(typeof(assignedgrps) == 'number') {
-      grps = [assignedgrps];
-    } else {
-      grps = assignedgrps.split(',');
-    }
-    $.each(grps, function(e,t){
-      if(t !="NoGroup") {
-        grp = allgroups["results"][t];
-        option = new Option(grp.text, grp.id, true, true)
-        thisuser.append(option).trigger('change');
-      }
     });
-    // manually trigger the `select2:select` event
-    thisuser.trigger({
-        type: 'select2:select',
-        params: {
-            allgroups: allgroups
-        }
-
-    });
-  });
+  userlisttable.buttons().container().appendTo('#UserList_wrapper .col-6:eq(0)');
+  //userlistrefresh();
   
   
 }
 initUserlist();
 
+
 var ipv4_address = $(".ipaddress");
 ipv4_address.inputmask();
 
-function refreshgroups() {
-  
-   var grpname;
-   userlistflag=1;
-   $("#Usergroups option").remove();
-   var grpname="dkfj"
-   $.each(allgroups, function(k,v){
-    grpname=allgroups[k]['name'].replace('usersigroup/','')
-    if (grpname!="Everyone") {
-     $("#Usergroups ").append("<option value='"+grpname+"' >"+grpname+"</option>");
-    }
-   });  
-  }
-
-function refreshUserList(){
- 
- 
-  if (typeof(allgroups)=="undefined" || typeof(allusers)=="undefined"){
-   return
-  }
-  console.log('allusers',allusers);
-  var jdata;
-  var username;
-  var usersize;
-  var userpool;
-  var evgroup;
-  var grpname;
-  var selecteds;
-  var grpuserlist;
-  var evuser; 
-
-
-
-  
-  //$(".selectpicker").selectpicker("refresh");
- 
- // $(".selectpicker").selectpicker("refresh");
- }
- 
- 
- $('.select2.multiple').select2({
-   ajax: {
-    url: 'api/v1/users/grouplist',
+function postdata(url,data){
+  $.ajax({
+    url: url,
     dataType: 'json',
-    // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
-    type: 'GET',
-    async: false,
-  }
-});
+    data: data,
+  });
+}
 
-$('.select2.pool').select2({
-  ajax: {
-   url: 'api/v1/pools/poolsinfo',
-   dataType: 'json',
-   // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
-   type: 'GET',
-   async: false,
- }
-});
-
+function selbtnclickeduser(x){
+  //$.post("./pump.php", { req:"UnixChangeUser", name:x.id.replace('btnsel',''), passwd:'groups'+$("#"+x.id.replace('btn','')).val()+" "+myname });
+        console.log('hi',x)
+       };
 $("#UnixAddUser").click( function (e){ 
   var apiurl = "api/v1/users/UnixAddUser";
   var apidata = {"name": $("#User").val(), "Volpool": $("#UserVol").val(), "groups":$("#Usergroups").val().toString(), 
             "Password": $("#UserPass").val(), "Volsize": $("#volsize").val(), 
             "HomeAddress": $("#HomeAddress").val(), "HomeSubnet": $("#HomeSubnet").val(), "Myname":"mezo"}
-  $.ajax({
-    url: apiurl,
-    dataType: 'json',
-    data: apidata,
-
-  });
+  postdata(apiurl,apidata);
  
   e.preventDefault();
-  console.log('hi',apiurl,apidata);
+  
 });
+
+function auserdel(){
+  var apiurl = "api/v1/users/userdel";
+  var apidata = {'name': arguments[0], 'Myname':'mezo'}
+  postdata(apiurl,apidata);
+};
+
+function refreshall(){
+  console.log('hi');
+
+  var newallgroups='new0';
+  $.ajax({
+    url: "api/v1/users/grouplist", 
+    type: "GET",
+    async: false,
+    //beforeSend: function(xhr){xhr.setRequestHeader('Access-Control-Allow-Origin', 'http://10.11.11.241:8080');},
+    
+    success: function(data) {  newallgroups=data; }
+   });
+   if(JSON.stringify(allgroups) != JSON.stringify(newallgroups)) {
+     allgroups = newallgroups; 
+     console.log('allgroupchange',allgroups,newallgroups)
+     groupsrefresh();
+   }
+
+  var newallpools = 'new0';
+  $.ajax({
+    url: "api/v1/pools/poolsinfo", 
+    type: "GET",
+    async: false,
+    //beforeSend: function(xhr){xhr.setRequestHeader('Access-Control-Allow-Origin', 'http://10.11.11.241:8080');},
+    
+    success: function(data) {  newallpools=data; }
+   });
+   if(JSON.stringify(allpools) != JSON.stringify(newallpools)) { 
+      allpools = newallpools;
+      poolsrefresh();
+    }
+  
+
+  var newallusers = 'new0';
+  $.ajax({
+    url: 'api/v1/users/userlist',
+    async: false,
+    type: 'GET',
+    dataSrc: 'allusers',
+    success: function(data) {  newallusers=data; }
+  });
+  if(JSON.stringify(allusers) != JSON.stringify(newallusers)){ 
+    allusers = newallusers;
+    userlistrefresh(); 
+  }
+}
+setInterval(refreshall, 2000);
