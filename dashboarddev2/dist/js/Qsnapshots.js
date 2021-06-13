76d8454fec5c1a4f98a4fc189673fcbf22e0bc50
dkjfa@sldjf <dkjfa@sldjf>
2021-06-13 17:57:27 +0300

    fixing

--- dashboarddev2/dist/js/Qsnapshots.js
+++ dashboarddev2/dist/js/Qsnapshots.js
@@ -1,3 +1,255 @@
+var refresherprop=2;
+var refresheruser=2;
+var userpass="hi";
+var proptime="55:55:55";
+var olddata=0;
+var propdata='hi';
+var oldproprdata="dakfj";
+var chartcards = [ 'quota', 'used', 'usedbysnapshots'];
+var charts = {}
+var proptimenew="33:333:33";
+var prop={};
+var prop2={};
+var selprop=0
+var hostips={} 
+var DNS=1;
+var oldcurrentinfo='dlkfajsdl;';
+ var redflag="";
+ var mydate;
+ var tempvar;
+ var allusers="init";
+ var allusersnohome="init";
+ var allvolumes = "init";
+ var allgroups= "";
+ var allpools= 'init';
+ var allsnaps = 'init';
+ var selvalues={};
+ var grpolddata;
+ var myidhash;
+ var mytimer;
+ var mymodal;
+ var cgrp={};
+ var cuser={};
+ var volumelistflag=0;
+ var userdata="dksfj";
+ var olduserdata="ksksksks";
+ var voldata='hihihi';
+ var oldvoldata='n;nolnlnn';
+ var volumes={'NoHome': 'NoHome'};
+ var idletill=480000;
+ var oldhdata="dkd";
+ var oldpdata="dkedfd";
+ var oldddata="dkjlf";
+ var oldrdata="kfld";
+ var volstats = 'init'
+ var stat = 'quota'
+ var selhosts="";
+ var seldhosts="";
+ var changedprop = {};
+ var modaltill=idletill-120000
+ var onceinittable;
+ var cpool = 'init';
+ var cvolume = 'init';
+ var dirtylog = 1;
+
+function postdata(url,data){
+  $.ajax({
+    url: url,
+    dataType: 'json',
+    timeout: 3000,
+    data: data,
+  });
+}
+ function poolsrefresh(){
+ 
+  $('.select2.pool').select2({
+    placeholder: "Select a pool",
+    ajax: {
+     url: '/api/v1/volumes/poolsinfo',
+     dataType: 'json',
+     timeout: 3000,
+     // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+     type: 'GET',
+     async: false,
+     success: function(data){ allpools = data;}
+   }
+  });
+  
+ }
+ poolsrefresh();
+
+
+function volumesrefresh(){
+  var newallvolumes = '';
+  var reload = 0
+  if($("#Pool2").val() == '') {  newallvolumes = '';}
+  else{
+    $.ajax({
+      url: '/api/v1/volumes/volumelist',
+      dataType: 'json',
+      timeout: 3000,
+      // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+      type: 'GET',
+      async: false,
+      success: function(data){ newallvolumes = data;}
+    });
+    if(JSON.stringify(allvolumes) != JSON.stringify(newallvolumes)) {
+      allvolumes = JSON.parse(JSON.stringify(newallvolumes)); 
+      newallvolumes=[];
+      $.each(allvolumes,function(e,t){
+        if(allvolumes[e]['pool'] == allpools['results'][$("#Pool2").val()]['text']){
+          newallvolumes.push(t);
+        }
+      });
+      reload = 1
+    }
+  }
+  if(cpool != $("#Pool2").val()){
+    cpool = $("#Pool2").val();
+    reload = 1
+  } 
+  $('.select2.volume').select2({
+    placeholder: "Select a volume",
+    data: newallvolumes
+  });
+
+
+  }
+function getsnaps(){
+  var newsnaps;
+  $.ajax({
+    url: 'api/v1/volumes/snapshots/snapshotsinfo',
+    timeout: 3000,
+    async: false,
+    type: 'GET',
+    success: function(data){  newsnaps = data}
+  });
+  return newsnaps
+}
+allsnaps = getsnaps();
+
+function onceinit(){
+
+  onceinittable=$("#Oncetable").DataTable({
+      "order": [[ 0, "desc" ],[ 1, "desc" ]],
+      "data": allsnaps['Once'],
+      "columns": [
+        {data: "date"}, {data:"time"},{data: "name" }, 
+        {data: null,
+         render: function(data, type, row){
+           return row.volume.split('_')[0]
+         }
+        },
+        {data: "used"}, {data:"refcompressratio"}, 
+        {
+          data: null,
+          render: function(data, type, row){
+            return '<a class="snapdelegt" val="username" href="javascript:rollback(\''+row.name+'\')" >'
+            + '<img  src="dist/img/return.png" data-name='+row.name+' alt="cannott upload delete icon">'
+            + '</a>';          }
+        },
+        {
+          data: null,
+          render: function(data, type, row){
+            return '<a class="snapdelegt" val="username" href="javascript:asnapdel(\''+row.name+'\')" >'
+            + '<img  src="dist/img/delete.png" data-name='+row.name+' alt="cannott upload delete icon">'
+            + '</a>'; 
+          }
+        },
+      ],
+      'columnDefs': [
+        {
+            'createdCell':  function (td, cellData, rowData, row, col) {
+                $(td).data('grps', 'cell-' + cellData); 
+            }
+        }
+      ],
+      
+    });
+    onceinittable.buttons().container().appendTo('#Oncetable_wrapper .col-6:eq(0)');
+  //volumelistrefresh();
+  
+}
+onceinit();
+
+
+function snapsreferesh(){
+  var newsnaps = getsnaps();
+  if(JSON.stringify(allsnaps) != JSON.stringify(newsnaps)) {
+    allsnaps = JSON.parse(JSON.stringify(newsnaps)); 
+    onceinittable.clear();
+    onceinittable.rows.add(allsnaps['Once'])
+    onceinittable.draw()
+  }
+   
+}
+function refreshall(){
+  snapsreferesh();
+}
+setInterval(refreshall,2000);
+
+
+
+
+
+
+
+
+
+
+
+function rollback(csnap){
+  var apiurl = "api/v1/volumes/snapshots/snaprollback";
+  var apidata = {"name": csnap, 'user':'mezo' }
+  postdata(apiurl,apidata);
+}
+
+function asnapdel(csnap){
+  var apiurl = "api/v1/volumes/snapshots/snapshotdel";
+  var apidata = {"name": csnap, 'user':'mezo' }
+  postdata(apiurl,apidata);
+}
+
+$("#oncecreate").click(function(e){
+e.preventDefault();
+var thepool = allpools['results'][$("#Pool2").val()]['text'];
+var owner = allpools['results'][$("#Pool2").val()]['owner'];
+var thevol = allvolumes[$("#volname").val()]['fullname'];
+var thesnap = $("#Oncename").val();
+
+var apiurl = "api/v1/volumes/snapshots/create";
+var apidata = {"snapsel": 'Once', "pool": thepool, "volume": thevol, 'name': thesnap, 'owner':owner }
+
+postdata(apiurl,apidata);
+
+
+});
+
+
+function changeoncesubmit(){
+  if($("#Oncename").val().length < 3) { 
+    $("#oncecreate").attr('disabled','disabled');
+   } else {
+     $("#oncecreate").attr('disabled',false);
+   }; 
+}
+
+ 
+$("#Pool2").change(function(e){   volumesrefresh(); });
+$("#volname").change(function(e){
+  cvolume = $("#volname").val();
+  if($("#volname").val() == ''){
+    $("#Oncename").attr('disabled','disabled');
+    $("#oncecreate").attr('disabled','disabled');
+  } else {
+    $("#Oncename").attr('disabled',false);
+    changeoncesubmit();
+  }
+});
+$("#Oncename").keyup(function(e){  
+  changeoncesubmit();
+});
+
 $(function () {
   $('#timepicker').datetimepicker({
     format: 'LT'
@@ -65,11 +317,3 @@ $(function () {
       });
 })
 
-var example1_filter = $(".example1_filter");
-$(".example1").DataTable({
-    //"responsive": true, "lengthChange": true, "autoWidth": true, "info":true,
-    "order": [[ 1, "desc" ]],
-    //"buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-  }).buttons().container().appendTo('.example1_wrapper .col-6:eq(0)');
-  //$("#example1_filter").css("margin-left","10rem");
-//$("#example1_filter").css("margin-left","10rem");
