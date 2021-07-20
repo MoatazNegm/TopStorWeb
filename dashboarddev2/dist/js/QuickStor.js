5a16f0c5eeb4541615b811fb3196b786ad9428a9
dlsafj@#fdk <dlsafj@#fdk>
2021-07-20 13:22:04 +0300

    fixing

--- dashboarddev2/dist/js/QuickStor.js
+++ dashboarddev2/dist/js/QuickStor.js
@@ -2,43 +2,69 @@
 var allhosts= {};
 var allhostsready = -1;
 var allhostsactive = -1;
-var thostcolor = 'red'
+var thost = 0;
+var thostcolor = 'red';
+var allusers= 0;
+var allconns = 0;
+var tusercolor = 'blue';
 alldgs = 'init';
 var totalstorage = 0;
 var totalstoragealloc = 0;
-var emergency = { 0:'darkblue', 25 : '#687ee6', 50: 'yellow', 75: 'eb2a45', 100: 'red'}
+var emergency = { 0:'darkblue', 25 : '#687ee6', 50: 'yellow', 75: 'eb2a45', 100: 'red'};
+var tstorage = 0;
 var tstoragecolor = 'grey';
 col = 1;
-function getdata(url){
+function getdata(url, data={}){
   var newdgs;
+  data['token'] = hypetoken
   $.ajax({
     url: url,
     timeout: 3000,
     async: false,
     type: 'GET',
+    data: data,
     success: function(data){  newdgs = data}
   });
-
   return newdgs
 }
 
 function extracthosts(){
      var newallhosts = getdata('api/v1/hosts/allinfo');
-     var thost = 0;
+     
      if(allhostsready != newallhosts['ready'].length || allhostsactive != newallhosts['active'].length){
         allhostsready = newallhosts['ready'].length ;
         allhostsactive = newallhosts['active'].length;
         $("#totalhosts").text(allhostsactive);
-        thost = allhostsready / allhostsactive;
+        thost = allhostsready ;
         $.each(emergency,function(e,t){
                 
-            if(100*(thost) > e) { thostcolor = emergency[100-e]}
+            if(100*(thost/allhostsactive) > e) { thostcolor = emergency[100-e]}
         });
-        $(".thost").val(thost);  
+       
     }   
-    $(".thost").trigger('configure', {'fgColor': thostcolor, 'min':0, 'max': 1, 'skin':'tron'});
+    $(".thost").trigger('configure', {'fgColor': thostcolor, 'min':0, 'max': allhostsactive, 'skin':'tron'});
+    $(".thost").val(thost);  
+    $(".thost").trigger('change'); 
     
 
+    
+
+}
+function extractconns(){
+    var newallconns = getdata('api/v1/volumes/connections');
+    var newallusers = getdata('api/v1/users/userlist');
+ 
+    var tconn = 0;
+    if(allusers != newallusers['allusers'].length || allconns.length != newallconns['connections'].length){
+       allusers = newallusers['allusers'];
+       allconns = newallconns['connections'];
+       $("#totalusers").text(allusers.length);
+       tuser = allconns.length;
+      
+   }   
+   $(".tuser").trigger('configure', {'fgColor': emergency[0], 'min':0, 'max': allusers.length, 'skin':'tron'});
+   $(".tuser").val(tuser);
+   $(".tuser").trigger('change');  
 }
 function extractstorage(){
     var newtotalstorage = 0;
@@ -54,20 +80,22 @@ function extractstorage(){
             change = 1;
             totalstorage = newtotalstorage
             totalstoragealloc = newtotalstoragealloc
-            tstorage = totalstoragealloc * 100 / totalstorage
+            tstorage = totalstoragealloc
             $.each(emergency,function(e,t){
                 
                 if(tstorage > e) { tstoragecolor = t}
             });
-            totalstorage = totalstorage.toString().slice(0,5);
-            tstorage = tstorage.toString().slice(0,5);
+            totalstorage = parseFloat(totalstorage.toString().slice(0,5));
+            tstorage = parseFloat(tstorage.toString().slice(0,5));
             
         }
     });
-        if(change > 0){
-        $("#TotalStorage").text(totalstorage+'GB');
-        $(".tstorage").val(tstorage);  
-        }   
+ 
+    $("#TotalStorage").text(totalstorage+'GB');
+    $(".tstorage").trigger('configure', {'fgColor': tstoragecolor, 'min':0, 'max': totalstorage *1000, 'skin':'tron'});
+    $(".tstorage").val(tstorage*1000);
+    $(".tstorage").trigger('change');
+    
         
 }
 //$(".addraid").click(function(){ console.log(this); $(this).find('input').prop('checked','checked'); });
@@ -79,8 +107,9 @@ extracthosts()
 function refreshall(){
     
     extractstorage();
-    extracthosts()
-    $(".tstorage").trigger('configure', {'fgColor': tstoragecolor});
+    extracthosts();
+    extractconns();
+    //$(".tstorage").trigger('configure', {'fgColor': tstoragecolor});
 
   
   ;
