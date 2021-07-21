32baf674a29ad6a9069d29c92024ce80ad7137b9
dlsafj@#fdk <dlsafj@#fdk>
2021-07-21 14:43:28 +0300

    fixing

--- dashboarddev2/dist/js/QuickStor.js
+++ dashboarddev2/dist/js/QuickStor.js
@@ -28,7 +28,36 @@ function getdata(url, data={}){
   });
   return newdgs
 }
+function extracttrends(){
+    var newtrends = getdata('api/v1/volumes/stats')['trends'];
+    trendstamps = [];
+    trendsizes = []
+    var trendict = {}
+    var tt;
+    $.each(newtrends,function(vol,trends){
+        if(trends != -1){
+            $.each(trends.split('/'),function(e,trend){
+                tt = Date(trend.split('-')[0]);
+                tt = new Date(tt).getMinutes(); 
+                trendict[tt] = []
+            });
+        }
+    });
+    var tt;
+    $.each(newtrends,function(vol,trends){
+        if(trends != -1){
+            $.each(trends.split('/'),function(e,trend){
+                tt = Date(trend.split('-')[0]);
+                tt = new Date(tt).getMinutes(); 
+                trendict[tt].push(trend.split('-')[1]); 
+            });
+        }
+    });
 
+    //console.log(trendstamps,trendsizes);
+    console.log(trendict);
+
+}
 function extracthosts(){
      var newallhosts = getdata('api/v1/hosts/allinfo');
      
@@ -132,7 +161,8 @@ function extractstorage(){
             newtotalstorage = newtotalstorage + pool['alloc'] + pool['available']; 
             newtotalstoragealloc = newtotalstoragealloc + pool['alloc'];
         }
-        if( newtotalstorage != totalstorage || newtotalstoragealloc != totalstoragealloc ){
+    });
+        
             change = 1;
             totalstorage = newtotalstorage
             totalstoragealloc = newtotalstoragealloc
@@ -144,9 +174,8 @@ function extractstorage(){
             totalstorage = parseFloat(totalstorage.toString().slice(0,5));
             tstorage = parseFloat(tstorage.toString().slice(0,5));
             
-        }
-    });
-    
+        
+
     $("#TotalStorage").text(totalstorage+'GB');
     $(".tstorage").trigger('configure', {'fgColor': tstoragecolor, 'min':0, 'max': totalstorage *1000, 'skin':'tron'});
     $(".tstorage").val(tstorage*1000);
@@ -173,5 +202,235 @@ function refreshall(){
   
   ;
 }
+refreshall();
 
-setInterval(refreshall,2000);
+setInterval(refreshall,10000);
+refreshall();
+
+
+
+/* jQueryKnob */
+
+$('.knob').knob({
+  /*change : function (value) {
+   //console.log("change : " + value);
+   },
+   release : function (value) {
+   console.log("release : " + value);
+   },
+   cancel : function () {
+   console.log("cancel : " + this.value);
+   },*/
+  draw: function () {
+
+    // "tron" case
+    if (this.$.data('skin') == 'tron') {
+
+      var a   = this.angle(this.cv)  // Angle
+        ,
+          sa  = this.startAngle          // Previous start angle
+        ,
+          sat = this.startAngle         // Start angle
+        ,
+          ea                            // Previous end angle
+        ,
+          eat = sat + a                 // End angle
+        ,
+          r   = true
+
+      this.g.lineWidth = this.lineWidth
+
+      this.o.cursor
+      && (sat = eat - 0.3)
+      && (eat = eat + 0.3)
+
+      if (this.o.displayPrevious) {
+        ea = this.startAngle + this.angle(this.value)
+        this.o.cursor
+        && (sa = ea - 0.3)
+        && (ea = ea + 0.3)
+        this.g.beginPath()
+        this.g.strokeStyle = this.previousColor
+        this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sa, ea, false)
+        this.g.stroke()
+      }
+
+      this.g.beginPath()
+      this.g.strokeStyle = r ? this.o.fgColor : this.fgColor
+      this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sat, eat, false)
+      this.g.stroke()
+
+      this.g.lineWidth = 2
+      this.g.beginPath()
+      this.g.strokeStyle = this.o.fgColor
+      this.g.arc(this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false)
+      this.g.stroke()
+
+      return false
+    }
+  }
+});
+/* END JQUERY KNOB */
+
+//-------------
+//- LINE CHART -
+//--------------
+var areaChartData = {
+  labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label               : 'Digital Goods',
+      backgroundColor     : 'rgba(60,141,188,0.9)',
+      borderColor         : 'rgba(60,141,188,0.8)',
+      pointRadius          : false,
+      pointColor          : '#3b8bba',
+      pointStrokeColor    : 'rgba(60,141,188,1)',
+      pointHighlightFill  : '#fff',
+      pointHighlightStroke: 'rgba(60,141,188,1)',
+      data                : [28, 48, 40, 19, 86, 27, 90]
+    },
+    {
+      label               : 'Electronics',
+      backgroundColor     : 'rgba(210, 214, 222, 1)',
+      borderColor         : 'rgba(210, 214, 222, 1)',
+      pointRadius         : false,
+      pointColor          : 'rgba(210, 214, 222, 1)',
+      pointStrokeColor    : '#c1c7d1',
+      pointHighlightFill  : '#fff',
+      pointHighlightStroke: 'rgba(220,220,220,1)',
+      data                : [65, 59, 80, 81, 56, 55, 40]
+    },
+  ]
+}
+
+var areaChartOptions = {
+  maintainAspectRatio : false,
+  responsive : true,
+  legend: {
+    display: false
+  },
+  scales: {
+    xAxes: [{
+      gridLines : {
+        display : false,
+      }
+    }],
+    yAxes: [{
+      gridLines : {
+        display : false,
+      }
+    }]
+  }
+}
+var lineChartCanvas = $('#lineChart').get(0).getContext('2d')
+var lineChartOptions = $.extend(true, {}, areaChartOptions)
+var lineChartData = $.extend(true, {}, areaChartData)
+lineChartData.datasets[0].fill = false;
+lineChartData.datasets[1].fill = false;
+lineChartOptions.datasetFill = false
+
+var lineChart = new Chart(lineChartCanvas, {
+  type: 'line',
+  data: lineChartData,
+  options: lineChartOptions
+})
+/*
+ * Flot Interactive Chart
+ * -----------------------
+ */
+// We use an inline data source in the example, usually data would
+// be fetched from a server
+var data        = [],
+    totalPoints = 100
+
+function getRandomData() {
+
+  if (data.length > 0) {
+    data = data.slice(1)
+  }
+
+  // Do a random walk
+  while (data.length < totalPoints) {
+
+    var prev = data.length > 0 ? data[data.length - 1] : 50,
+        y    = prev + Math.random() * 10 - 5
+
+    if (y < 0) {
+      y = 0
+    } else if (y > 100) {
+      y = 100
+    }
+
+    data.push(y)
+  }
+
+  // Zip the generated y values with the x values
+  var res = []
+  for (var i = 0; i < data.length; ++i) {
+    res.push([i, data[i]])
+  }
+
+  return res
+}
+
+var interactive_plot = $.plot('#interactive', [
+    {
+      data: getRandomData(),
+    }
+  ],
+  {
+    grid: {
+      borderColor: '#f3f3f3',
+      borderWidth: 1,
+      tickColor: '#f3f3f3'
+    },
+    series: {
+      color: '#3c8dbc',
+      lines: {
+        lineWidth: 2,
+        show: true,
+        fill: true,
+      },
+    },
+    yaxis: {
+      min: 0,
+      max: 100,
+      show: true
+    },
+    xaxis: {
+      show: true
+    }
+  }
+)
+
+var updateInterval = 500 //Fetch data ever x milliseconds
+var realtime       = 'on' //If == to on then fetch data every x seconds. else stop fetching
+function update() {
+
+  interactive_plot.setData([getRandomData()])
+
+  // Since the axes don't change, we don't need to call plot.setupGrid()
+  interactive_plot.draw()
+  if (realtime === 'on') {
+    setTimeout(update, updateInterval)
+  }
+}
+
+//INITIALIZE REALTIME DATA FETCHING
+if (realtime === 'on') {
+  update()
+}
+//REALTIME TOGGLE
+$('#realtime .btn').click(function () {
+  if ($(this).data('toggle') === 'on') {
+    realtime = 'on'
+  }
+  else {
+    realtime = 'off'
+  }
+  update()
+})
+/*
+ * END INTERACTIVE CHART
+ */
+
