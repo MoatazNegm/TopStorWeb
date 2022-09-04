726c3d33f1f25adc2239540e4916addf5425c76f
YousefAllam221B <yousef.allam221b@gmail.com>
2022-09-05 00:30:16 +0200

    pumping waiting fixed

--- dist/js/QLogind.js
+++ dist/js/QLogind.js
@@ -57,13 +57,15 @@ if (typeof Storage !== "undefined") {
 
 $("button").click(function (e) {
 	$("#Loading").toggle();
-	urlapi = "api/v1/login";
-	datapi = { user: $("#user").val(), pass: $("#pass").val() };
-	localStorage.setItem("user", $("#user").val());
-	$("#user").prop("disabled", true);
-	$("#pass").prop("disabled", true);
-	$("#signin").hide();
-	postlogin(urlapi, datapi);
+	setTimeout(function () {
+		urlapi = "api/v1/login";
+		datapi = { user: $("#user").val(), pass: $("#pass").val() };
+		localStorage.setItem("user", $("#user").val());
+		$("#user").prop("disabled", true);
+		$("#pass").prop("disabled", true);
+		$("#signin").hide();
+		postlogin(urlapi, datapi);
+	}, 20);
 });
 
 dirtylog = 1;
