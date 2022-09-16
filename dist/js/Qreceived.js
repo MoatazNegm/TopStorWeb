6dbf0e47b594142af05f4c5254688a170c7f34f3
YousefAllam221B <yousef.allam221b@gmail.com>
2022-09-17 00:40:07 +0200

    Working on filter table

--- dist/js/Qreceived.js
+++ dist/js/Qreceived.js
@@ -321,7 +321,7 @@ function initalltables() {
 		},
 	});
 
-	console.log(filterstable.columns);
+	console.log(filterstable["allsnaps"].columns);
 	if (JSON.stringify(allsnaps) != JSON.stringify(newsnaps)) {
 		allsnaps = JSON.parse(JSON.stringify(newsnaps));
 		allpsnapstable["allsnaps"].clear();
