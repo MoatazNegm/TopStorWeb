75a2f81d0e67aa5c2458dd581de909bda31058a8
YousefAllam221B <yousef.allam221b@gmail.com>
2022-09-17 00:09:01 +0200

    Working on filter table

--- dist/js/Qreceived.js
+++ dist/js/Qreceived.js
@@ -25,38 +25,56 @@ function poolsrefresh() {
 }
 poolsrefresh();
 
+// function volumesrefresh() {
+// 	var newallvolumes = "";
+// 	var reload = 0;
+// 	if ($("#Pool2").val() == "") {
+// 		newallvolumes = "";
+// 	} else {
+// 		$.ajax({
+// 			url: "/api/v1/volumes/volumelist",
+// 			dataType: "json",
+// 			timeout: 3000,
+// 			// Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+// 			type: "GET",
+// 			async: false,
+// 			success: function (data) {
+// 				newallvolumes = data;
+// 			},
+// 		});
+// 		if (JSON.stringify(allvolumes) != JSON.stringify(newallvolumes)) {
+// 			allvolumes = JSON.parse(JSON.stringify(newallvolumes));
+// 			newallvolumes = [];
+// 			$.each(allvolumes, function (e, t) {
+// 				if (allvolumes[e]["pool"] == allpools["results"][$("#Pool2").val()]["text"]) {
+// 					newallvolumes.push(t);
+// 				}
+// 			});
+// 			reload = 1;
+// 		}
+// 	}
+// 	if (cpool != $("#Pool2").val()) {
+// 		cpool = $("#Pool2").val();
+// 		reload = 1;
+// 	}
+// 	$(".select2.volume").select2({
+// 		placeholder: "Select a volume",
+// 		data: newallvolumes,
+// 	});
+// }
+
 function volumesrefresh() {
-	var newallvolumes = "";
-	var reload = 0;
-	if ($("#Pool2").val() == "") {
-		newallvolumes = "";
-	} else {
-		$.ajax({
-			url: "/api/v1/volumes/volumelist",
-			dataType: "json",
-			timeout: 3000,
-			// Additional AJAX parameters go here; see the end of this chapter for the full code of this example
-			type: "GET",
-			async: false,
-			success: function (data) {
-				newallvolumes = data;
-			},
-		});
-		if (JSON.stringify(allvolumes) != JSON.stringify(newallvolumes)) {
-			allvolumes = JSON.parse(JSON.stringify(newallvolumes));
-			newallvolumes = [];
-			$.each(allvolumes, function (e, t) {
-				if (allvolumes[e]["pool"] == allpools["results"][$("#Pool2").val()]["text"]) {
-					newallvolumes.push(t);
-				}
-			});
-			reload = 1;
-		}
-	}
-	if (cpool != $("#Pool2").val()) {
-		cpool = $("#Pool2").val();
-		reload = 1;
-	}
+	$.ajax({
+		url: "/api/v1/volumes/volumelist",
+		dataType: "json",
+		timeout: 3000,
+		// Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+		type: "GET",
+		async: false,
+		success: function (data) {
+			newallvolumes = data;
+		},
+	});
 	$(".select2.volume").select2({
 		placeholder: "Select a volume",
 		data: newallvolumes,
@@ -64,36 +82,54 @@ function volumesrefresh() {
 }
 volumesrefresh();
 
+// function partnersrefresh() {
+// 	var newallpartners = "";
+// 	var reload = 0;
+// 	if ($("#volname").val() == "") {
+// 		newallpartners = "";
+// 	} else {
+// 		$.ajax({
+// 			url: "/api/v1/partners/partnerlist",
+// 			dataType: "json",
+// 			timeout: 3000,
+// 			// Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+// 			type: "GET",
+// 			async: false,
+// 			success: function (data) {
+// 				newallpartners = data;
+// 			},
+// 		});
+// 		if (JSON.stringify(allvolumes) != JSON.stringify(newallpartners)) {
+// 			allpartners = JSON.parse(JSON.stringify(newallpartners));
+// 			newallpartners = [];
+// 			$.each(allpartners["allpartners"], function (e, t) {
+// 				if (t["type"].includes("ender") > 0) {
+// 					t["text"] = t["alias"].split("_")[0];
+// 					t["id"] = e;
+// 					newallpartners.push(t);
+// 				}
+// 			});
+// 			reload = 1;
+// 		}
+// 	}
+// 	$(".select2.Sender").select2({
+// 		placeholder: "Select a partner",
+// 		data: newallpartners,
+// 	});
+// }
+
 function partnersrefresh() {
-	var newallpartners = "";
-	var reload = 0;
-	if ($("#volname").val() == "") {
-		newallpartners = "";
-	} else {
-		$.ajax({
-			url: "/api/v1/partners/partnerlist",
-			dataType: "json",
-			timeout: 3000,
-			// Additional AJAX parameters go here; see the end of this chapter for the full code of this example
-			type: "GET",
-			async: false,
-			success: function (data) {
-				newallpartners = data;
-			},
-		});
-		if (JSON.stringify(allvolumes) != JSON.stringify(newallpartners)) {
-			allpartners = JSON.parse(JSON.stringify(newallpartners));
-			newallpartners = [];
-			$.each(allpartners["allpartners"], function (e, t) {
-				if (t["type"].includes("ender") > 0) {
-					t["text"] = t["alias"].split("_")[0];
-					t["id"] = e;
-					newallpartners.push(t);
-				}
-			});
-			reload = 1;
-		}
-	}
+	$.ajax({
+		url: "/api/v1/partners/partnerlist",
+		dataType: "json",
+		timeout: 3000,
+		// Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+		type: "GET",
+		async: false,
+		success: function (data) {
+			newallpartners = data;
+		},
+	});
 	$(".select2.Sender").select2({
 		placeholder: "Select a partner",
 		data: newallpartners,
