5af9486934f232fda51e73c1805eed5403d5e286
YousefAllam221B <yousef.allam221b@gmail.com>
2022-09-17 02:50:05 +0200

    adding Any record for all lists

--- dist/js/Qreceived.js
+++ dist/js/Qreceived.js
@@ -88,6 +88,7 @@ function volumesrefresh() {
 			newallvolumes = [];
 			$.each(allvolumes, function (e, t) {
 				console.log(allvolumes[e]["pool"]);
+                console.log("#Pool2").val());
 				console.log(allpools["results"][$("#Pool2").val() + 1]);
 				console.log(allpools["results"][$("#Pool2").val() + 1]["text"]);
 
