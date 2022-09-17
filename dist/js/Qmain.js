d12d956e0e0d13dcae108d0700e9b779eeedd55c
dlksj.ewjf <dklljfd@sldjf.com>
2022-09-17 20:41:38 +0300

    fxlfkjl;

--- dist/js/Qmain.js
+++ dist/js/Qmain.js
@@ -248,6 +248,7 @@ var bg = {
 setInterval(function () {
 	var notif;
 	onedaylogfn();
+	$(".modal-backdrop").css('z-index','0')
 	$.ajax({
 		url: "api/v1/info/notification",
 		async: true,
@@ -292,6 +293,7 @@ $("body").click(function (e) {
 	var apiurl = "api/v1/login/renewtoken";
 	var apidata = { token: hypetoken };
 	postdata(apiurl, apidata);
+	$(".modal-backdrop").css('z-index','0')
 });
 $("li.auths a.active").css("background-color", "#4558FC");
 $(".chgpasswd").click(function (e) {
@@ -310,4 +312,6 @@ $(".chpass").focusout(function (e) {
 		$("#passerr").css("color", "white");
 		$("#passwrd").prop("disabled", false);
 	}
+	$(".modal-backdrop").css('z-index','0')
 });
+$(".modal-backdrop").css('z-index','0')
