8a32c1e6c649af9354d8f8d0f9bddcc157fc79a3
sdlfkj <dkdjkl@dljkf.com>
2023-07-17 15:16:01 +0300

    css and button appears and disappears

--- dist/js/viewPasswordButton.js
+++ dist/js/viewPasswordButton.js
@@ -1,8 +1,21 @@
 var ShowPasswordTogglePass = document.querySelector("#pass");
-ShowPasswordTogglePass.onclick = function() {
-    document.querySelector("#pass").classList.add("input-password");
-    document.getElementById("toggle-password-pass").classList.remove("d-none");
-};
+//ShowPasswordTogglePass.onclick = function() {
+//    document.querySelector("#pass").classList.add("input-password");
+//    document.getElementById("toggle-password-pass").classList.remove("d-none");
+//};
+
+
+function showOrHideButton(){
+        if (passwordInputPass.value.length > 0) {
+                document.querySelector("#pass").classList.add("input-password");
+                document.getElementById("toggle-password-pass").classList.remove("d-none");
+    } else {
+    document.querySelector("#pass").classList.remove("input-password");
+    document.getElementById("toggle-password-pass").classList.add("d-none");
+    }
+}
+
+ShowPasswordTogglePass.addEventListener("keyup", showOrHideButton);
 const passwordInputPass = document.querySelector("#pass");
 const togglePasswordButtonPass = document.getElementById("toggle-password-pass");
 togglePasswordButtonPass.addEventListener("click", togglePassword);
@@ -34,4 +47,3 @@ function togglePasswordNewpass() {
         togglePasswordButtonNewpass.setAttribute("aria-label", "Show password as plain text.")
     }
 }
-~                                                                                                                                                                       ~                                                                                                  
